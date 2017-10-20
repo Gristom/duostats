a05cf23a6d223c892661aa15677e5b3831798376
Gristom <31354244+Gristom@users.noreply.github.com>
2017-10-20 15:56:56 -0400

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -53,15 +53,15 @@ from decimal import Decimal, ROUND_HALF_UP, ROUND_UP
 
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
+##@app.errorhandler(404)
+##def not_found_error(error):
+##    return render_template('404.html'), 404
+##
+##
+##@app.errorhandler(500)
+##def internal_error(error):
+##    db.session.rollback()
+##    return render_template('500.html'), 500
 
 
 
